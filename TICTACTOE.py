ffa01a16de2944ef0907e68354b61e08f5db7472
TheBase99 <87952798+TheBase99@users.noreply.github.com>
2022-05-27 11:59:16 +0000

    Add files via upload

---
+++ TICTACTOE.py
@@ -0,0 +1,31 @@
+print('What is your name ?')
+name = input()
+message = 'Hope You Enjoy'
+
+for i in message:
+	print(i.center(10,'*'))
+print('Hello\n' + name + '\nThanks for trying out my game')
+
+theBoard = {'top-L': ' ','top-M': ' ','top-R': ' ',
+            'mid-L': ' ','mid-M': ' ','mid-R': ' ',
+            'low-L': ' ','low-M': ' ','low-R': ' '}
+print('Use top-L, top-M, top-R', 'mid-L','mid-M', 'mid-R', 'low-L','low-M','low-R')
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'O'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+
